986812f64fac7eba56f2d2224fc97fe061be2d9d
AndreyScript <a.krutko777@gmail.com>
2021-02-24 19:38:13 +0200

    add 16

---
+++ lesson 16 JS/src/js/script.js
@@ -0,0 +1,20 @@
+let firstNumber = prompt ('Введите первое число');
+let secondNumber = prompt ('Введите второе число');
+let result = firstNumber * secondNumber;
+alert (result);
+
+let name = prompt ('Введите имя: ');
+let lastName = prompt ('Введите фамилию: ');
+let performance = prompt('Выступление:');
+let theatre = prompt('Театр:');
+let sector = prompt('Сектор:');
+let row = prompt('Ряд:');
+let place = prompt('Место:');
+let time = prompt('Время:');
+
+let ticket = `Имя: ${name} \nФамилия: ${lastName} \nВыступление: ${performance} \nТеатр: ${theatre} \nСектор: ${sector} \nВаш ряд: ${row} \nВаше место: ${place} \nВремя: ${time}`
+
+alert (ticket);
+
+
+
