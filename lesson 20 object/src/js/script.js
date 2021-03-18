7f48f09a9a464396d900a3b78d2910ddf79b2d86
AndreyScript <a.krutko777@gmail.com>
2021-03-18 06:55:51 +0200

    20 added

--- lesson 20 object/src/js/script.js
+++ lesson 20 object/src/js/script.js
@@ -1,3 +1,5 @@
+/*1.*/
+
 let obj = {
     bread: 25,
     milk: 30,
@@ -6,9 +8,60 @@ let obj = {
     cucumbers: 42
 }
 
-let arr = Object.values(obj);
-let min = Math.min(arr);
-let max = Math.max(arr);
-/*let sum = Math.*/
+let sum = 0;
+for (let products in obj) {
+    sum += +obj[products];
+}
+console.log('Общая стоимость покупок ' + sum);
 
-console.log( `Min value: ${min}, max value: ${max}` );
+let arr = Object.keys( obj ).map(function ( key ) { return obj[key]; });
+let min = Math.min.apply( null, arr );
+console.log('Cамый дешевый товар ' + min);
+let max = Math.max.apply( null, arr );
+console.log('Cамый дорогой товар ' + max);
+
+/*2.*/
+
+let films =[ {
+    title: 'Joker',
+    rating: 8.1,
+    premier: true
+} ,
+{
+    title: 'Iron Man',
+    rating: 7,
+    premier: true
+} ,
+{
+    title: 'Thor',
+    rating: 7.5,
+    premier: false
+},
+{
+    title: 'Black Panther',
+    rating: 8.2,
+    premier: true
+} ,
+{
+    title: 'Hulk',
+    rating: 8.5,
+    premier: false
+} ]
+
+let ratingSum = 0;
+let itemsFound = 0;
+let item = null;
+for (let i = 0; i < films.length; i++) {
+    item = films[i];
+    if (item.premier || !item.premier) {
+        ratingSum = item.rating + ratingSum;
+        itemsFound = itemsFound + 1;
+    } if (item.premier) {
+        console.log (films[i]);
+    } if (item.rating > 8.0) {
+        console.log (item.title + ' -', item.rating);
+    }
+}
+    let averagerating = ratingSum / itemsFound;
+    console.log("Средняя оценка: " + averagerating);
+
